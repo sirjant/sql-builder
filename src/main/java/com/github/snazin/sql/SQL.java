5d6c6ee05b1c26014f6949c135431027dca9c9bb
Sergey Nazin <sirjant0038@gmail.com>
2018-09-08 14:48:35 +0300

    Add 'select' and 'from' simple implementation

---
+++ src/main/java/com/github/snazin/sql/SQL.java
@@ -0,0 +1,39 @@
+package com.github.snazin.sql;
+
+
+import org.intellij.lang.annotations.Language;
+
+import static org.apache.commons.lang3.StringUtils.join;
+
+
+public class SQL {
+
+    @Language("SQL")
+    private String query = "";
+
+    public SQL() {
+    }
+
+    public SQL(@Language("SQL") String query) {
+        this.query = query;
+    }
+
+    public static SQL create() {
+        return new SQL();
+    }
+
+    public SQL select(String... fields) {
+        query = "select " + join(fields, ", ");
+        return new SQL(query);
+    }
+
+    public SQL from(String... tables) {
+        query = query + " \nfrom " + join(tables, ", ");
+        return new SQL(query);
+    }
+
+    @Override
+    public String toString() {
+        return query;
+    }
+}
